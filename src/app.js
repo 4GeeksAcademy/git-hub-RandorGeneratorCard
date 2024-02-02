0e065f2cafb47b3821ab9b97b4855075911671af
clisdermar <clisdermar@gmail.com>
2024-02-02 04:29:26 +0000

    Generador de Cartas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // /* eslint-disable */
 // import "bootstrap";
-// import "./style.css";
+import "./style.css";
 
 // import "./assets/img/rigo-baby.jpg";
 // import "./assets/img/4geeks.ico";
@@ -10,6 +10,8 @@
 //   console.log("Hello Rigo from the console!");
 // };
 // Datos para las cartas
+
+let intervalId;
 let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 let ranks = [
   "2",
@@ -26,21 +28,23 @@ let ranks = [
   "King",
   "Ace"
 ];
-
+// generador aleatorio de cartas
 function generateRandomCard() {
   let randomSuit = suits[Math.floor(Math.random() * suits.length)];
   let randomRank = ranks[Math.floor(Math.random() * ranks.length)];
 
+  // cree un objeto y retorne 2 propiedades de el para manipular mejor
   return {
     suit: randomSuit,
     rank: randomRank
   };
 }
+// PAra obtener la imagen desde la carpeta en donde almacene
 
 function getCardImageSrc(card) {
-  let cardFileName =
+  let cardName =
     card.rank.toLowerCase() + "_of_" + card.suit.toLowerCase() + ".png";
-  return "src/images/" + cardFileName;
+  return "src/images/" + cardName;
 }
 
 function generateCardHTML(card) {
@@ -49,10 +53,21 @@ function generateCardHTML(card) {
   cardImage.alt = `${card.rank} of ${card.suit}`;
 }
 
+function startInterval() {
+  let intervalValue = parseInt(document.getElementById("intervalInput").value);
+  // Limpiar el intervalo actual antes de comenzar uno nuevo
+  clearInterval(intervalId);
+  // Configurar un nuevo intervalo
+  intervalId = setInterval(generateCard, intervalValue * 1000);
+}
+startInterval();
+
+// Declaro un funcion general
 function generateCard() {
   let randomCard = generateRandomCard();
   generateCardHTML(randomCard);
 }
 
+document.getElementById("btn").addEventListener("click", generateCard);
 // Llamar a generateCard al cargar la página para mostrar una carta inicial
 generateCard();
